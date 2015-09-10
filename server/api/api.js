7c9a700b0c223248631766a364598fdfcc2ac04a
wonderxboy <wonderxboy@gmail.com>
2015-09-11 00:06:38 +0200

    Rest api custom auth prototype

---
+++ server/api/api.js
@@ -0,0 +1,60 @@
+Boards = new Mongo.Collection('board');
+
+  // Global API configuration
+  var Api = new Restivus({
+    useDefaultAuth: false,
+    auth: {
+      user: function() {
+        if (this.queryParams.token === Meteor.settings.authToken) {
+          return { user : Meteor.users.findOne({"username": "mike"})};
+        }
+        else {
+          return null;
+        }
+      }
+    },
+    prettyJson: true,
+    onLoggedIn: function () {
+      console.log(this.user.username + ' (' + this.userId + ') logged in');
+    },
+  });
+
+  // Generates: GET, POST on /api/items and GET, PUT, DELETE on
+  // /api/items/:id for the Items collection
+  Api.addCollection(Boards);
+
+  // Generates: POST on /api/users and GET, DELETE /api/users/:id for
+  // Meteor.users collection
+  Api.addCollection(Meteor.users, {
+    //excludedEndpoints: ['getAll', 'put'],
+    routeOptions: {
+      authRequired: false
+    },
+    endpoints: {
+      post: {
+        authRequired: false
+      },
+      delete: {
+        roleRequired: 'admin'
+      }
+    }
+  });
+
+  // Maps to: /api/articles/:id
+  Api.addRoute('boards/:id', {authRequired: true}, {
+    get: function () {
+      return Boards.findOne(this.urlParams.id);
+    },
+    delete: {
+      roleRequired: ['author', 'admin'],
+      action: function () {
+        if (Articles.remove(this.urlParams.id)) {
+          return {status: 'success', data: {message: 'Article removed'}};
+        }
+        return {
+          statusCode: 404,
+          body: {status: 'fail', message: 'Article not found'}
+        };
+      }
+    }
+  });
